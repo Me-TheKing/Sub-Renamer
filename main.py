d1f20903ed92704e7c00e90b899a0e3c4a31a446
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-17 22:59:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ from PyQt5.QtCore import QFileInfo
 from UI.maingui import Ui_Form  # importing our generated file
 
 
-class MyApp(QtWidgets.QMainWindow, Ui_Form):
+class MyApp(QtWidgets.QWidget):
     def __init__(self):
-        super(MyApp, self).__init__()
+        QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+                
         # call the all the btn methods
         self.btn_handler()        
 
     def btn_handler(self):
-        	self.ui.addfile_btn.clicked.connect(self.addfile_mth)
-        	self.ui.preview_btn.clicked.connect(self.preview_mth)
+        self.ui.addfile_btn.clicked.connect(self.preview_mth)
+        #self.ui.preview_btn.clicked.connect(self.preview_mth)
 
     def addfile_mth(self):
+        print(self.sender().text())
         try:            
             # QFileDialog.getOpenFileName(self, [Title], [Directory], "[some filters]")
             # leave the dirctory blank to start the app from the running folder
-            fileNames = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test", "All Filles (*)")[0]
+            fileNames, _ = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test", "All Filles (*)")
+            
             if fileNames:
             	for name in fileNames :
             		fileName = QFileInfo(name).fileName()
+            		path = QFileInfo(name).path()
+            		#print(path)
             		item = QtWidgets.QListWidgetItem()
             		item.setText(fileName)
             		item.setCheckState(QtCore.Qt.Checked)
@@ -33,18 +38,24 @@ class MyApp(QtWidgets.QMainWindow, Ui_Form):
             pass
 
     def preview_mth(self):
-    	checked_items = []
-    	for index in range(self.ui.listWidget.count()):
-    		if self.ui.listWidget.item(index).checkState() == QtCore.Qt.Checked:
-    			checked_items.append(self.ui.listWidget.item(index).text())
+    	sender_btn = self.sender().text()
+    	if sender_btn == "Add File(s)":
+    		self.addfile_mth()
+    	elif sender_btn == "Add Folder":
+    		pass
+    	else :
+	    	checked_items = []
+	    	for index in range(self.ui.listWidget.count()):
+	    		if self.ui.listWidget.item(index).checkState() == QtCore.Qt.Checked:
+	    			checked_items.append(self.ui.listWidget.item(index).text())
 
-    	model = QtGui.QStandardItemModel()
-    	self.ui.listView.setModel(model)
+	    	model = QtGui.QStandardItemModel()
+	    	self.ui.listView.setModel(model)
 
-    	for i in checked_items:
-    		item = QtGui.QStandardItem(i)
+	    	for i in checked_items:
+	    		item = QtGui.QStandardItem(i)
 
-    		model.appendRow(item)
+	    		model.appendRow(item)
 
     def moveUP(self):
     	currentRow = self.ui.listWidget.currentRow()
