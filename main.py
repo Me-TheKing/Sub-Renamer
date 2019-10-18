3d4ea9fed66ecc3e82e7333fd94d7a64202fa5e6
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-19 02:01:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from UI.maingui import Ui_Form  # importing our generated file
 
 class MyApp(QtWidgets.QWidget):
     def __init__(self):
-        #super(self, MyApp).__init__()
+        # super(self, MyApp).__init__()
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         # call the all the btn methods
         self.btn_handler()
+        # set my var
+        self.path_memory_id =[]
 
     def btn_handler(self):
         self.ui.addfile_btn.clicked.connect(self.btn_sender)
@@ -22,52 +24,69 @@ class MyApp(QtWidgets.QWidget):
         sender_btn = self.sender().text()
 
         if sender_btn == "Add File(s)":
-            path_memory_id = self.addfile_mth()
+            self.path_memory_id.append(self.addfile_mth())
+            self.preview_mth()
         elif sender_btn == "Add Folder":
             pass
-
-        if sender_btn == "Preview" and path_memory_id:
-            self.preview_mth(path_memory_id)
+        elif sender_btn == "Preview":
+            self.preview_mth()
 
     def addfile_mth(self):
         try:
             # QFileDialog.getOpenFileName(self, [Title], [Directory], "[some filters]")
             # leave the dirctory blank to start the app from the running folder
-            fileNames, _ = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test", "All Filles (*)")
+            fileNames, _ = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test",
+                                                        "All Filles (*)")
 
             if fileNames:
-                path_memory_id = list
-                for name in fileNames :
-                    fileName: str = QFileInfo(name).fileName()
-                    path = QFileInfo(name).path()
-                    # add item(s) to listwidget
-                    item = QtWidgets.QListWidgetItem()
-                    item.setText(fileName)
-                    item.setCheckState(QtCore.Qt.Checked)
-                    self.ui.listWidget.addItem(item)
-                    # add the path and it's name  memory Id location in a list
-                    memory_id = str(item).split(" ")[-1][:-1]
-                    path_memory_id.append([path, memory_id])
+                path_memoryid = []
+                for name in fileNames:
+                    if len(self.path_memory_id):
+                        for old_name, _, _ in self.path_memory_id:
+                            if name == old_name:
+                                print("you olready have this file in your list")
+                            else:
+                                fileName = QFileInfo(name).fileName()
+                                path = QFileInfo(name).path()
+                                # add item(s) to listwidget
+                                item = QtWidgets.QListWidgetItem()
+                                item.setText(fileName)
+                                item.setCheckState(QtCore.Qt.Checked)
+                                self.ui.listWidget.addItem(item)
+                                # add the path and it's name  memory Id location in a list
+                                memory_id = str(item).split(" ")[-1][:-1]
+                                path_memoryid.append([name, path, memory_id])
         except OSError:
             pass
-        finally:
-            return path_memory_id
 
-    def preview_mth(self, path_memory_id):
+        if path_memoryid:
+            return path_memoryid
+
+    def preview_mth(self):
+        print(self.path_memory_id)
         # add item(s) to the listview (part01)
         model = QtGui.QStandardItemModel()
         self.ui.listView.setModel(model)
 
-        for index in range(self.ui.listWidget.count()):
-            if self.ui.listWidget.item(index).checkState() == QtCore.Qt.Checked:
+        total_row = self.ui.listWidget.count()
+        a_row = self.ui.listWidget.item
+
+        for index in range(total_row):
+            if a_row(index).checkState() == QtCore.Qt.Checked:
                 # add item(s) to the listview (part02) with the rename option(s)
-                item = self.ui.listWidget.item(index).text()
+                item = QtGui.QStandardItem(a_row(index).text())
                 model.appendRow(item)
             else:
                 # add item(s) to the listview (part02) don't rename
-                item = self.ui.listWidget.item(index).text()
-                item.format()
+                item = QtGui.QStandardItem(a_row(index).text())
+                # i need to just color the text to gray
                 model.appendRow(item)
+
+    # def moveUP(self):
+    #     currentRow = self.ui.listWidget.currentRow()
+    #     currentItem = self.ui.listWidget.takeItem(currentRow)
+    #     self.ui.listWidget.insertItem(currentRow - 1, currentItem)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
