2983fffa0c993d494e0e88d23d7a0befa5eb7adf
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-19 10:39:57 +0300

    add log_cobox for the history log(s) add unrename_btn for undo rename add clear_btn to clear the listwedgit and the listview add date_chbox option to add date column to the original list add type_chbox option to add file type column to the original list change the design a little

--- main.py
+++ main.py
@@ -19,48 +19,53 @@ class MyApp(QtWidgets.QWidget):
     def btn_handler(self):
         self.ui.addfile_btn.clicked.connect(self.btn_sender)
         self.ui.preview_btn.clicked.connect(self.btn_sender)
+        self.ui.clear_btn.clicked.connect(self.clear_mth)
 
     def btn_sender(self):
         sender_btn = self.sender().text()
 
         if sender_btn == "Add File(s)":
-            self.path_memory_id.append(self.addfile_mth())
-            self.preview_mth()
+            result = self.addfile_mth()
+            if result:
+                self.path_memory_id = result.copy()
+                self.preview_mth()
         elif sender_btn == "Add Folder":
             pass
         elif sender_btn == "Preview":
             self.preview_mth()
 
     def addfile_mth(self):
-        try:
-            # QFileDialog.getOpenFileName(self, [Title], [Directory], "[some filters]")
-            # leave the dirctory blank to start the app from the running folder
-            fileNames, _ = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test",
-                                                        "All Filles (*)")
+        # QFileDialog.getOpenFileName(self, [Title], [Directory], "[some filters]")
+        # leave the dirctory blank to start the app from the running folder
+        fileNames, _ = QFileDialog.getOpenFileNames(self, "Get File(s)", "C:\\Users\\H.Ali\\Desktop\\Rename test",
+                                                    "All Filles (*);; Video Filles (*.mkv, *.mp4, *.avi, *.ts, *.m4v)")
+        path_memoryid = []
+        duplicate = False
+        if fileNames:
+            for name in fileNames:
+                if len(self.path_memory_id):
+                    for old_name, _, _ in self.path_memory_id:
+                        if name == old_name:
+                            duplicate = True
+                            print("you olready have this file in your list")
+                            break
 
-            if fileNames:
-                path_memoryid = []
-                for name in fileNames:
-                    if len(self.path_memory_id):
-                        for old_name, _, _ in self.path_memory_id:
-                            if name == old_name:
-                                print("you olready have this file in your list")
-                            else:
-                                fileName = QFileInfo(name).fileName()
-                                path = QFileInfo(name).path()
-                                # add item(s) to listwidget
-                                item = QtWidgets.QListWidgetItem()
-                                item.setText(fileName)
-                                item.setCheckState(QtCore.Qt.Checked)
-                                self.ui.listWidget.addItem(item)
-                                # add the path and it's name  memory Id location in a list
-                                memory_id = str(item).split(" ")[-1][:-1]
-                                path_memoryid.append([name, path, memory_id])
-        except OSError:
-            pass
+                if not duplicate:
+                    fileName = QFileInfo(name).fileName()
+                    path = QFileInfo(name).path()
+                    # add item(s) to listwidget
+                    item = QtWidgets.QListWidgetItem()
+                    item.setText(fileName)
+                    item.setCheckState(QtCore.Qt.Checked)
+                    self.ui.listWidget.addItem(item)
+                    # add the path and it's memory ID location in a list
+                    memory_id = str(item).split(" ")[-1][:-1]
+                    path_memoryid.append([name, path, memory_id])
 
         if path_memoryid:
             return path_memoryid
+        else:
+            return False
 
     def preview_mth(self):
         print(self.path_memory_id)
@@ -71,16 +76,40 @@ class MyApp(QtWidgets.QWidget):
         total_row = self.ui.listWidget.count()
         a_row = self.ui.listWidget.item
 
+        # some out of the loop option(s)
+        if self.ui.serial_LE.text():
+            serial = int(self.ui.serial_LE.text())
+
         for index in range(total_row):
             if a_row(index).checkState() == QtCore.Qt.Checked:
+                name_ext = a_row(index).text().split(".")
+
+                if self.ui.name_LE.text():
+                    name_ext[0] = self.ui.name_LE.text()
+                if self.ui.ext_LE.text():
+                    name_ext[1] = self.ui.ext_LE.text()
+
                 # add item(s) to the listview (part02) with the rename option(s)
-                item = QtGui.QStandardItem(a_row(index).text())
+                if self.ui.serial_LE.text():
+                    item = QtGui.QStandardItem(f"{name_ext[0]} {str(serial).zfill(len(self.ui.serial_LE.text()))}.{name_ext[1]}")
+                    serial += 1
+                else:
+                    item = QtGui.QStandardItem(f"{name_ext[0]}.{name_ext[1]}")
+
                 model.appendRow(item)
             else:
                 # add item(s) to the listview (part02) don't rename
                 item = QtGui.QStandardItem(a_row(index).text())
                 # i need to just color the text to gray
                 model.appendRow(item)
+
+    def clear_mth(self):
+        # clear all the item(s) in listwidget
+        self.ui.listWidget.clear()
+        # clear all the item(s) in listview
+        model = QtGui.QStandardItemModel()
+        self.ui.listView.setModel(model)
+        model.removeRows(0, model.rowCount())
 
     # def moveUP(self):
     #     currentRow = self.ui.listWidget.currentRow()
